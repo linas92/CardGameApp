38d7dc34643537837a23b4a4bda71a9acd656673
Linas Norkus <linasnorkus92@gmail.com>
2023-06-29 10:40:46 +0300

    good enough, not going to scale it more, need to learn other stuff. Peace ♥♦♣♠

--- Program.cs
+++ Program.cs
@@ -9,19 +9,46 @@ namespace CardGameApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"What do you want to play?\nType 0 for Blackjack\nType 1 for Poker\nAnything else to exit");
 
-            PokerDeck deck = new PokerDeck();
-            
+            string playerInput = Console.ReadLine();
+
+            if (playerInput == "0")
+            {
+                BlackJackGame();
+            }
+            else if (playerInput == "1")
+            {
+                PokerGame();
+            }
+            else
+            {
+                Console.WriteLine("Good day");
+            }
+
+            Console.ReadLine();
+        }
+        public static void BlackJackGame()
+        {
+            BlackjackDeck deck = new BlackjackDeck();
+
             var hand = deck.DealCards();
 
             foreach (var card in hand)
             {
-                Console.WriteLine($"{card.Suit} {card.Value}");
+                Console.WriteLine($"{card.Value} of {card.Suit}");
             }
+        }
+        public static void PokerGame()
+        {
+            PokerDeck deck = new PokerDeck();
 
+            var hand = deck.DealCards();
 
-
-            Console.ReadLine();
+            foreach (var card in hand)
+            {
+                Console.WriteLine($"{card.Value} of {card.Suit}");
+            }
         }
     }
 }
