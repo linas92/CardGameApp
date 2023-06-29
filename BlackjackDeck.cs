a025d5880f9d9ea15b5fd08fe8572a094886603c
Linas Norkus <linasnorkus92@gmail.com>
2023-06-29 09:59:36 +0300

    created a blackjack deck class

---
+++ BlackjackDeck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CardGameApp
+{
+    public class BlackjackDeck : Deck
+    {
+        public override List<PlayingCard> DealCards()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
